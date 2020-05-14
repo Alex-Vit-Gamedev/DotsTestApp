1e839f349351fac0dbc7e71ab3b210deaf91d5ff
Alex Vit <vit.gamedev.acc@gmail.com>
2020-05-14 14:54:26 +0700

    inittial commit

---
+++ Assets/Scripts/Dot/DotIOHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// dot input-output control system
+/// </summary>
+[RequireComponent(typeof(Dot))]
+public class DotIOHandler : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private static bool _hasSelection;
+
+    private Dot _dot;
+    
+    //delegates for events
+    public delegate void OnSelectionStarted();
+    public delegate void OnDotSelected(Dot dot);
+    public delegate void OnSelectionEnded();
+    
+    //selection events
+    public event OnSelectionStarted SelectionStarted;
+    public event OnDotSelected DotSelected;
+    public event OnSelectionEnded SelectionEnded;
+    
+    private void Awake()
+    {
+        _dot = GetComponent<Dot>();
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!_hasSelection)
+        {
+            _hasSelection = true;
+            SelectionStarted?.Invoke();
+            OnPointerEnter(eventData);
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_hasSelection)
+        {
+            DotSelected?.Invoke(_dot);
+        }
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (_hasSelection)
+        {
+            _hasSelection = false;
+            SelectionEnded?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GameUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField] 
+    private Text scoreLabel;
+
+    [Header("Buttons")] 
+    [SerializeField] 
+    private Button clearSaveButton;
+
+    public event Action onClearSaveButtonClick;
+
+    private void Awake()
+    {
+        clearSaveButton.onClick.AddListener(() => onClearSaveButtonClick?.Invoke());
+    }
+
+    public void UpdateInfo(GameSession current)
+    {
+        if(current == null)
+            scoreLabel.text = GameData.scoreText;
+        else
+        {
+            scoreLabel.text = GameData.scoreText + current.scores.ToString();
+        }
+    }
+}
